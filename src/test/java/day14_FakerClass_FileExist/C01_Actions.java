dff1a66e0dc08148fd3de0f4866beaa76ad889df
kubraay25 <kubraaydgdu2525@gmail.com>
2023-04-18 22:24:43 +0300

    jh

---
+++ src/test/java/day14_FakerClass_FileExist/C01_Actions.java
@@ -0,0 +1,21 @@
+package day14_FakerClass_FileExist;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+public class C01_Actions extends TestBase {
+    @Test
+    public void actions() {
+        //"https://techproeducation.com/" gidin
+        driver.get("https://techproeducation.com/");
+        driver.findElement(By.cssSelector("i[class='eicon-close']")).click();
+        // İlk kelime SHIFT tuşuna basılı şekilde, ikinci kelime basılmadan "TECHPRO education" araması yapın.
+        WebElement search = driver.findElement(By.xpath("//*[@type='search']"));
+        Actions actions = new Actions(driver);
+
+
+
+    }
+}
